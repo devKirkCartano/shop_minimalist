d37ba5b8e1954aee057cdeed4cf637ff5b80605e
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-18 18:34:28 +0800

    initial commit

---
+++ connect_db.php
@@ -0,0 +1,17 @@
+<?php
+  $db_server_name = "localhost";
+  $db_user = "root";
+  $db_password = "";
+  $db_name = "shop_minimalist_db";
+  $conn = ""; // variable to store the connection
+
+  try{
+     // establish a connection to MYSQl database
+    $conn = mysqli_connect($db_server_name, $db_user, $db_password, $db_name);
+  } catch (mysqli_sql_exception){
+    // if cannot connect to database, show an alert
+    echo '<script language="javascript">';
+    echo 'alert("Cannot connect to database!")';
+    echo '</script>';
+  }
+?>
