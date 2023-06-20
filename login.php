aef6970ca35758b26c036580942c880d2e81db8d
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-20 14:34:01 +0800

    indent php script

--- login.php
+++ login.php
@@ -1,39 +1,39 @@
 <?php
-session_start();
+    session_start();
 
-if(  isset($_SESSION['username']) )
-{
-  header("location:home.php");
-  die();
-}
-//connect to database
-include("connect_db.php");
-if($db)
-{
-  if(isset($_POST['login_btn']))
-  {
-      $username=mysqli_real_escape_string($db,$_POST['username']);
-      $password=mysqli_real_escape_string($db,$_POST['password']);
-      $password=md5($password); //Remember we hashed password before storing last time
-      $sql="SELECT * FROM users WHERE  username='$username' AND password='$password'";
-      $result=mysqli_query($db,$sql);
-      
-      if($result)
-      {
-     
-        if( mysqli_num_rows($result)>=1)
+    if(  isset($_SESSION['username']) )
+    {
+    header("location:home.php");
+    die();
+    }
+    //connect to database
+    include("connect_db.php");
+    if($db)
+    {
+    if(isset($_POST['login_btn']))
+    {
+        $username=mysqli_real_escape_string($db,$_POST['username']);
+        $password=mysqli_real_escape_string($db,$_POST['password']);
+        $password=md5($password); //Remember we hashed password before storing last time
+        $sql="SELECT * FROM users WHERE  username='$username' AND password='$password'";
+        $result=mysqli_query($db,$sql);
+        
+        if($result)
         {
-            $_SESSION['message']="You are now Loggged In";
-            $_SESSION['username']=$username;
-            header("location:home.php");
+        
+            if( mysqli_num_rows($result)>=1)
+            {
+                $_SESSION['message']="You are now Loggged In";
+                $_SESSION['username']=$username;
+                header("location:home.php");
+            }
+        else
+        {
+                $_SESSION['message']="Username and Password combination incorrect";
         }
-       else
-       {
-              $_SESSION['message']="Username and Password combination incorrect";
-       }
-      }
-  }
-}
+        }
+    }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
