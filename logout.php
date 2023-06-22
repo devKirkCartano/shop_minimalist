58bcbeadaee8bc4ff94ca81cd615f5a1f2a1ac83
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-22 21:54:27 +0800

    added comment

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?php
-session_start(); //to ensure you are using same session
-session_destroy(); // destroy entire session
-unset($_SESSION['fname']);
-unset($_SESSION['lname']);
-unset($_SESSION['email']);
-$_SESSION['message']="You are now logged out";
-echo '<script>alert("'.$_SESSION['message'].'")</script>';
-echo '<script>window.location.href = "login.php";</script>'; // redirects to index.php
+session_start(); // start session with the user who logged in
+session_destroy(); // destroy entire session, because user is logging out
+unset($_SESSION['fname']); // remove the first name from session
+unset($_SESSION['lname']); // remove the last name from session
+unset($_SESSION['email']); // remove the email from session
+$_SESSION['message']="You are now logged out"; // 
+echo '<script>alert("'.$_SESSION['message'].'")</script>'; // display the message
+echo '<script>window.location.href = "login.php";</script>'; // redirects to login.php
 ?>
