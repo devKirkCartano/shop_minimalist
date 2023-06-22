49339c762fa321269eb33b232ad877ff8bb71ff4
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-22 20:57:14 +0800

    edit redirection page

--- logout.php
+++ logout.php
@@ -6,5 +6,5 @@ unset($_SESSION['lname']);
 unset($_SESSION['email']);
 $_SESSION['message']="You are now logged out";
 echo '<script>alert("'.$_SESSION['message'].'")</script>';
-echo '<script>window.location.href = "index.php";</script>'; // redirects to index.php
+echo '<script>window.location.href = "login.php";</script>'; // redirects to index.php
 ?>
