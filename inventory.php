3af5f55fd7806f454674eecaa4227eead190a42c
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-27 22:14:02 +0800

    fixed path

--- inventory.php
+++ inventory.php
@@ -293,7 +293,7 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
         </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/js/script.min.js?h=bdf36300aae20ed8ebca7e88738d5267"></script>
+    <script src="assets/js/script.min.js"></script>
     <script src="assets/js/script.min.js"></script>
     <!-- Bootstrap core JavaScript-->
     <script src="assets/js/jquery.min.js"></script>
