7f569727d82a4d7fe320e1e2cc78fe83a665734b
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-29 21:02:31 +0800

    removed duplicated script

--- inventory.php
+++ inventory.php
@@ -290,8 +290,6 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <!-- Core plugin JavaScript-->
     <script src="assets/jquery-easing/jquery.easing.min.js"></script>
-    <!-- Custom scripts for all pages-->
-    <script src="assets/js/sb-admin-2.min.js"></script>
     <!-- Page level plugins -->
     <script src="assets/datatables/jquery.dataTables.min.js"></script>
     <script src="assets/datatables/dataTables.bootstrap4.min.js"></script>
