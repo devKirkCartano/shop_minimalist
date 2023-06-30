5488ffed520f27fc538149360f9a0d0887c1d6fe
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-06-30 10:46:43 +0800

    removed duplicated script

--- index.php
+++ index.php
@@ -365,7 +365,6 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
-        <script src="/assets/js/script.min.js?h=bdf36300aae20ed8ebca7e88738d5267"></script>
         <script src="assets/js/script.min.js"></script>
     </body>
 
