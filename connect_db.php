ebed11ad1030d0bc6ff96dc2bc0aace9f34c945e
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-20 12:45:46 +0800

    added global variable for catch block

--- connect_db.php
+++ connect_db.php
@@ -8,7 +8,7 @@
   try{
      // try to establish a connection to MYSQl database
     $conn = mysqli_connect($db_server_name, $db_user, $db_password, $db_name);
-  } catch (mysqli_sql_exception){
+  } catch (mysqli_sql_exception $e){
     // if cannot connect to database, show an alert
     echo '<script>alert("Cannot connect to database!")</script>';
   }
