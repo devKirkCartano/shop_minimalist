365341821a09f8ca96b619f46d33dd234f56a0a2
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-26 21:36:25 +0800

    edit link for sales

--- product.php
+++ product.php
@@ -55,14 +55,14 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
                     <li class="nav-item"><a class="nav-link icon-color" href="index.php"><i
                                 class="fas fa-tachometer-alt icon-color"></i><span>Dashboard</span></a></li>
                     <li class="nav-item"></li>
-                    <li class="nav-item"><a class="nav-link active icon-color" href="product.php"><svg
+                    <li class="nav-item"><a class="nav-link active icon-color" href="salesInventory.php"><svg
                                 xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="1em"
                                 viewBox="0 0 24 24" width="1em" fill="currentColor" class="icon-color">
                                 <rect fill="none" height="24" width="24"></rect>
                                 <path
                                     d="M13,10h-2V8h2V10z M13,6h-2V1h2V6z M7,18c-1.1,0-1.99,0.9-1.99,2S5.9,22,7,22s2-0.9,2-2S8.1,18,7,18z M17,18 c-1.1,0-1.99,0.9-1.99,2s0.89,2,1.99,2s2-0.9,2-2S18.1,18,17,18z M8.1,13h7.45c0.75,0,1.41-0.41,1.75-1.03L21,4.96L19.25,4l-3.7,7 H8.53L4.27,2H1v2h2l3.6,7.59l-1.35,2.44C4.52,15.37,5.48,17,7,17h12v-2H7L8.1,13z">
                                 </path>
-                            </svg><span>Product</span></a><a class="nav-link icon-color" href="inventory.php"><i
+                            </svg><span>Sales</span></a><a class="nav-link icon-color" href="inventory.php"><i
                                 class="far fa-list-alt icon-color"></i><span>Inventory List</span></a></li>
                     <li class="nav-item"></li>
                     <li class="nav-item"></li>
