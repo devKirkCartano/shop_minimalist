00dee734dc1e1e4d33185c6122f2b27b0ebb0e38
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-26 14:25:09 +0800

    initial commit

---
+++ editProfile.php
@@ -0,0 +1,23 @@
+<?php
+// Include the database connection file
+require_once("connect_db.php");
+// Get the hidden ID field value
+
+if (isset($_POST['update-btn'])) {
+  // Escape special characters in a string for use in an SQL statement
+  $id = mysqli_real_escape_string($conn, $_POST['id']); // Assuming you have an input field with name="id" in the form.
+  $type = mysqli_real_escape_string($conn, $_POST['type']);
+  $model = mysqli_real_escape_string($conn, $_POST['model']);
+  $color = mysqli_real_escape_string($conn, $_POST['color']);
+  $quantity = mysqli_real_escape_string($conn, $_POST['quantity']);
+  $status = mysqli_real_escape_string($conn, $_POST['status']);
+
+  // Update the database table
+  $result = mysqli_query($conn, "UPDATE products SET `type` = '$type', `model` = '$model', `color` = '$color', `quantity` = $quantity, `status` = '$status', `last_updated` = CURRENT_TIMESTAMP WHERE `id` = $id");
+
+  // Display success message
+  echo '<script>alert("Data updated successfully!")</script>';
+  // Redirect to the view page
+  echo '<script>window.location.href = "inventory.php";</script>';
+}
+?>
