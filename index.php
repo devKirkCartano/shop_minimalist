e86db27c0b39d1f0f81bcaa6db7f9c2787b685fc
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-27 18:22:04 +0800

    commented pie chart

--- index.php
+++ index.php
@@ -1015,8 +1015,8 @@ chart.appear(1000, 100);
                             <div class="col-lg-5 col-xl-4">
                                 <div class="card shadow mb-4 graph-bg">
                                     <div class="card-header d-flex justify-content-between align-items-center header-graph">
-                                        <h6 class="text-primary fw-bold m-0 graph-text">Revenue Sources</h6>
-                                        <div class="dropdown no-arrow"><button class="btn btn-link btn-sm dropdown-toggle"
+                                        <h6 class="text-primary fw-bold m-0 graph-text">Stocks Sources</h6>
+                                        <!-- <div class="dropdown no-arrow"><button class="btn btn-link btn-sm dropdown-toggle"
                                                 aria-expanded="false" data-bs-toggle="dropdown" type="button"><i
                                                     class="fas fa-ellipsis-v text-gray-400"></i></button>
                                             <div class="dropdown-menu shadow dropdown-menu-end animated--fade-in">
@@ -1026,18 +1026,19 @@ chart.appear(1000, 100);
                                                 <div class="dropdown-divider"></div><a class="dropdown-item"
                                                     href="#">&nbsp;Something else here</a>
                                             </div>
-                                        </div>
+                                        </div> -->
                                     </div>
                                     <div class="card-body">
-                                        <div class="chart-area"><canvas
-                                                data-bss-chart="{&quot;type&quot;:&quot;doughnut&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;Direct&quot;,&quot;Social&quot;,&quot;Referral&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;&quot;,&quot;backgroundColor&quot;:[&quot;#4e73df&quot;,&quot;#1cc88a&quot;,&quot;#36b9cc&quot;],&quot;borderColor&quot;:[&quot;#ffffff&quot;,&quot;#ffffff&quot;,&quot;#ffffff&quot;],&quot;data&quot;:[&quot;50&quot;,&quot;30&quot;,&quot;15&quot;]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}}}"></canvas>
+                                        <div class="chart-area">
+                                            <!-- <canvas data-bss-chart="{&quot;type&quot;:&quot;doughnut&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;Direct&quot;,&quot;Social&quot;,&quot;Referral&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;&quot;,&quot;backgroundColor&quot;:[&quot;#4e73df&quot;,&quot;#1cc88a&quot;,&quot;#36b9cc&quot;],&quot;borderColor&quot;:[&quot;#ffffff&quot;,&quot;#ffffff&quot;,&quot;#ffffff&quot;],&quot;data&quot;:[&quot;50&quot;,&quot;30&quot;,&quot;15&quot;]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}}}"></canvas> -->
                                         </div>
-                                        <div class="text-center small mt-4"><span class="me-2 graph-text"><i
+                                        <!-- <div class="text-center small mt-4"><span class="me-2 graph-text"><i
                                                     class="fas fa-circle text-primary"></i>&nbsp;Direct</span><span
                                                 class="me-2 graph-text"><i
                                                     class="fas fa-circle text-success"></i>&nbsp;Social</span><span
                                                 class="me-2 graph-text"><i
-                                                    class="fas fa-circle text-info"></i>&nbsp;Refferal</span></div>
+                                                    class="fas fa-circle text-info"></i>&nbsp;Refferal</span>
+                                        </div> -->
                                     </div>
                                 </div>
                             </div>
