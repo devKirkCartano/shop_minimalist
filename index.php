29f9967c78cdafef4212b18041bba3fa1f5142d5
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-27 20:35:44 +0800

    integrate js to php

--- index.php
+++ index.php
@@ -8,10 +8,9 @@ $result = mysqli_query($conn, $sql); // execute the query
 $row = mysqli_fetch_assoc($result); // fetch the result, or data in a row
 
 // if user is logged in, it means that the session variables are set or contain values. Then display the dashboard
-if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['email']))
-{
-    
-?>
+if (isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['email'])) {
+
+    ?>
     <!DOCTYPE html>
     <html lang="en">
 
@@ -29,9 +28,1007 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
         <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
         <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
 
-        <script src="assets/js/piechart.js"></script>
-        <script src="assets/js/saleschart.js"></script>
+        <!-- Script for pie chart -->
+        <script>
+            am5.ready(function () {
+  // Create root1 element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root1 = am5.Root.new("pie");
 
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root1.setThemes([am5themes_Animated.new(root1)]);
+
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+  // start and end angle must be set both for chart and series
+  var chart = root1.container.children.push(
+    am5percent.PieChart.new(root1, {
+      layout: root1.verticalLayout,
+      innerRadius: am5.percent(40),
+    })
+  );
+
+  // Create series
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+  // start and end angle must be set both for chart and series
+  var series0 = chart.series.push(
+    am5percent.PieSeries.new(root1, {
+      valueField: "bottles",
+      categoryField: "country",
+      alignLabels: false,
+    })
+  );
+
+  var bgColor = root1.interfaceColors.get("background");
+
+  series0.ticks.template.setAll({ forceHidden: true });
+  series0.labels.template.setAll({ forceHidden: true });
+  series0.slices.template.setAll({
+    stroke: bgColor,
+    strokeWidth: 2,
+    tooltipText:
+      "{category}: {valuePercentTotal.formatNumber('0.00')}% ({value} bottles)",
+  });
+  series0.slices.template.states.create("hover", { scale: 0.95 });
+
+  var series1 = chart.series.push(
+    am5percent.PieSeries.new(root1, {
+      valueField: "litres",
+      categoryField: "country",
+      alignLabels: true,
+    })
+  );
+
+  series1.slices.template.setAll({
+    stroke: bgColor,
+    strokeWidth: 2,
+    tooltipText:
+      "{category}: {valuePercentTotal.formatNumber('0.00')}% ({value} litres)",
+  });
+
+  var data = [
+    {
+      country: "Lithuania",
+      litres: 501.9,
+      bottles: 1500,
+    },
+    {
+      country: "Czech Republic",
+      litres: 301.9,
+      bottles: 990,
+    },
+    {
+      country: "Ireland",
+      litres: 201.1,
+      bottles: 785,
+    },
+    {
+      country: "Germany",
+      litres: 165.8,
+      bottles: 255,
+    },
+    {
+      country: "Australia",
+      litres: 139.9,
+      bottles: 452,
+    },
+    {
+      country: "Austria",
+      litres: 128.3,
+      bottles: 332,
+    },
+    {
+      country: "UK",
+      litres: 99,
+      bottles: 150,
+    },
+    {
+      country: "Belgium",
+      litres: 60,
+      bottles: 178,
+    },
+    {
+      country: "The Netherlands",
+      litres: 50,
+      bottles: 50,
+    },
+  ];
+
+  // Set data
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+  series0.data.setAll(data);
+  series1.data.setAll(data);
+
+  // Play initial series animation
+  // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
+  series0.appear(1000, 100);
+  series1.appear(1000, 100);
+}); // end am5.ready()
+
+        </script>
+<!-- Script for sales chart -->
+
+    <script>
+        am5.ready(function () {
+  // Create root2 element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root2 = am5.Root.new("sales");
+
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root2.setThemes([am5themes_Animated.new(root2)]);
+
+  root2.dateFormatter.setAll({
+    dateFormat: "yyyy",
+    dateFields: ["valueX"],
+  });
+
+  var data = [
+    {
+      date: "2012-07-27",
+      value: 13,
+    },
+    {
+      date: "2012-07-28",
+      value: 11,
+    },
+    {
+      date: "2012-07-29",
+      value: 15,
+    },
+    {
+      date: "2012-07-30",
+      value: 16,
+    },
+    {
+      date: "2012-07-31",
+      value: 18,
+    },
+    {
+      date: "2012-08-01",
+      value: 13,
+    },
+    {
+      date: "2012-08-02",
+      value: 22,
+    },
+    {
+      date: "2012-08-03",
+      value: 23,
+    },
+    {
+      date: "2012-08-04",
+      value: 20,
+    },
+    {
+      date: "2012-08-05",
+      value: 17,
+    },
+    {
+      date: "2012-08-06",
+      value: 16,
+    },
+    {
+      date: "2012-08-07",
+      value: 18,
+    },
+    {
+      date: "2012-08-08",
+      value: 21,
+    },
+    {
+      date: "2012-08-09",
+      value: 26,
+    },
+    {
+      date: "2012-08-10",
+      value: 24,
+    },
+    {
+      date: "2012-08-11",
+      value: 29,
+    },
+    {
+      date: "2012-08-12",
+      value: 32,
+    },
+    {
+      date: "2012-08-13",
+      value: 18,
+    },
+    {
+      date: "2012-08-14",
+      value: 24,
+    },
+    {
+      date: "2012-08-15",
+      value: 22,
+    },
+    {
+      date: "2012-08-16",
+      value: 18,
+    },
+    {
+      date: "2012-08-17",
+      value: 19,
+    },
+    {
+      date: "2012-08-18",
+      value: 14,
+    },
+    {
+      date: "2012-08-19",
+      value: 15,
+    },
+    {
+      date: "2012-08-20",
+      value: 12,
+    },
+    {
+      date: "2012-08-21",
+      value: 8,
+    },
+    {
+      date: "2012-08-22",
+      value: 9,
+    },
+    {
+      date: "2012-08-23",
+      value: 8,
+    },
+    {
+      date: "2012-08-24",
+      value: 7,
+    },
+    {
+      date: "2012-08-25",
+      value: 5,
+    },
+    {
+      date: "2012-08-26",
+      value: 11,
+    },
+    {
+      date: "2012-08-27",
+      value: 13,
+    },
+    {
+      date: "2012-08-28",
+      value: 18,
+    },
+    {
+      date: "2012-08-29",
+      value: 20,
+    },
+    {
+      date: "2012-08-30",
+      value: 29,
+    },
+    {
+      date: "2012-08-31",
+      value: 33,
+    },
+    {
+      date: "2012-09-01",
+      value: 42,
+    },
+    {
+      date: "2012-09-02",
+      value: 35,
+    },
+    {
+      date: "2012-09-03",
+      value: 31,
+    },
+    {
+      date: "2012-09-04",
+      value: 47,
+    },
+    {
+      date: "2012-09-05",
+      value: 52,
+    },
+    {
+      date: "2012-09-06",
+      value: 46,
+    },
+    {
+      date: "2012-09-07",
+      value: 41,
+    },
+    {
+      date: "2012-09-08",
+      value: 43,
+    },
+    {
+      date: "2012-09-09",
+      value: 40,
+    },
+    {
+      date: "2012-09-10",
+      value: 39,
+    },
+    {
+      date: "2012-09-11",
+      value: 34,
+    },
+    {
+      date: "2012-09-12",
+      value: 29,
+    },
+    {
+      date: "2012-09-13",
+      value: 34,
+    },
+    {
+      date: "2012-09-14",
+      value: 37,
+    },
+    {
+      date: "2012-09-15",
+      value: 42,
+    },
+    {
+      date: "2012-09-16",
+      value: 49,
+    },
+    {
+      date: "2012-09-17",
+      value: 46,
+    },
+    {
+      date: "2012-09-18",
+      value: 47,
+    },
+    {
+      date: "2012-09-19",
+      value: 55,
+    },
+    {
+      date: "2012-09-20",
+      value: 59,
+    },
+    {
+      date: "2012-09-21",
+      value: 58,
+    },
+    {
+      date: "2012-09-22",
+      value: 57,
+    },
+    {
+      date: "2012-09-23",
+      value: 61,
+    },
+    {
+      date: "2012-09-24",
+      value: 59,
+    },
+    {
+      date: "2012-09-25",
+      value: 67,
+    },
+    {
+      date: "2012-09-26",
+      value: 65,
+    },
+    {
+      date: "2012-09-27",
+      value: 61,
+    },
+    {
+      date: "2012-09-28",
+      value: 66,
+    },
+    {
+      date: "2012-09-29",
+      value: 69,
+    },
+    {
+      date: "2012-09-30",
+      value: 71,
+    },
+    {
+      date: "2012-10-01",
+      value: 67,
+    },
+    {
+      date: "2012-10-02",
+      value: 63,
+    },
+    {
+      date: "2012-10-03",
+      value: 46,
+    },
+    {
+      date: "2012-10-04",
+      value: 32,
+    },
+    {
+      date: "2012-10-05",
+      value: 21,
+    },
+    {
+      date: "2012-10-06",
+      value: 18,
+    },
+    {
+      date: "2012-10-07",
+      value: 21,
+    },
+    {
+      date: "2012-10-08",
+      value: 28,
+    },
+    {
+      date: "2012-10-09",
+      value: 27,
+    },
+    {
+      date: "2012-10-10",
+      value: 36,
+    },
+    {
+      date: "2012-10-11",
+      value: 33,
+    },
+    {
+      date: "2012-10-12",
+      value: 31,
+    },
+    {
+      date: "2012-10-13",
+      value: 30,
+    },
+    {
+      date: "2012-10-14",
+      value: 34,
+    },
+    {
+      date: "2012-10-15",
+      value: 38,
+    },
+    {
+      date: "2012-10-16",
+      value: 37,
+    },
+    {
+      date: "2012-10-17",
+      value: 44,
+    },
+    {
+      date: "2012-10-18",
+      value: 49,
+    },
+    {
+      date: "2012-10-19",
+      value: 53,
+    },
+    {
+      date: "2012-10-20",
+      value: 57,
+    },
+    {
+      date: "2012-10-21",
+      value: 60,
+    },
+    {
+      date: "2012-10-22",
+      value: 61,
+    },
+    {
+      date: "2012-10-23",
+      value: 69,
+    },
+    {
+      date: "2012-10-24",
+      value: 67,
+    },
+    {
+      date: "2012-10-25",
+      value: 72,
+    },
+    {
+      date: "2012-10-26",
+      value: 77,
+    },
+    {
+      date: "2012-10-27",
+      value: 75,
+    },
+    {
+      date: "2012-10-28",
+      value: 70,
+    },
+    {
+      date: "2012-10-29",
+      value: 72,
+    },
+    {
+      date: "2012-10-30",
+      value: 70,
+    },
+    {
+      date: "2012-10-31",
+      value: 72,
+    },
+    {
+      date: "2012-11-01",
+      value: 73,
+    },
+    {
+      date: "2012-11-02",
+      value: 67,
+    },
+    {
+      date: "2012-11-03",
+      value: 68,
+    },
+    {
+      date: "2012-11-04",
+      value: 65,
+    },
+    {
+      date: "2012-11-05",
+      value: 71,
+    },
+    {
+      date: "2012-11-06",
+      value: 75,
+    },
+    {
+      date: "2012-11-07",
+      value: 74,
+    },
+    {
+      date: "2012-11-08",
+      value: 71,
+    },
+    {
+      date: "2012-11-09",
+      value: 76,
+    },
+    {
+      date: "2012-11-10",
+      value: 77,
+    },
+    {
+      date: "2012-11-11",
+      value: 81,
+    },
+    {
+      date: "2012-11-12",
+      value: 83,
+    },
+    {
+      date: "2012-11-13",
+      value: 80,
+    },
+    {
+      date: "2012-11-14",
+      value: 81,
+    },
+    {
+      date: "2012-11-15",
+      value: 87,
+    },
+    {
+      date: "2012-11-16",
+      value: 82,
+    },
+    {
+      date: "2012-11-17",
+      value: 86,
+    },
+    {
+      date: "2012-11-18",
+      value: 80,
+    },
+    {
+      date: "2012-11-19",
+      value: 87,
+    },
+    {
+      date: "2012-11-20",
+      value: 83,
+    },
+    {
+      date: "2012-11-21",
+      value: 85,
+    },
+    {
+      date: "2012-11-22",
+      value: 84,
+    },
+    {
+      date: "2012-11-23",
+      value: 82,
+    },
+    {
+      date: "2012-11-24",
+      value: 73,
+    },
+    {
+      date: "2012-11-25",
+      value: 71,
+    },
+    {
+      date: "2012-11-26",
+      value: 75,
+    },
+    {
+      date: "2012-11-27",
+      value: 79,
+    },
+    {
+      date: "2012-11-28",
+      value: 70,
+    },
+    {
+      date: "2012-11-29",
+      value: 73,
+    },
+    {
+      date: "2012-11-30",
+      value: 61,
+    },
+    {
+      date: "2012-12-01",
+      value: 62,
+    },
+    {
+      date: "2012-12-02",
+      value: 66,
+    },
+    {
+      date: "2012-12-03",
+      value: 65,
+    },
+    {
+      date: "2012-12-04",
+      value: 73,
+    },
+    {
+      date: "2012-12-05",
+      value: 79,
+    },
+    {
+      date: "2012-12-06",
+      value: 78,
+    },
+    {
+      date: "2012-12-07",
+      value: 78,
+    },
+    {
+      date: "2012-12-08",
+      value: 78,
+    },
+    {
+      date: "2012-12-09",
+      value: 74,
+    },
+    {
+      date: "2012-12-10",
+      value: 73,
+    },
+    {
+      date: "2012-12-11",
+      value: 75,
+    },
+    {
+      date: "2012-12-12",
+      value: 70,
+    },
+    {
+      date: "2012-12-13",
+      value: 77,
+    },
+    {
+      date: "2012-12-14",
+      value: 67,
+    },
+    {
+      date: "2012-12-15",
+      value: 62,
+    },
+    {
+      date: "2012-12-16",
+      value: 64,
+    },
+    {
+      date: "2012-12-17",
+      value: 61,
+    },
+    {
+      date: "2012-12-18",
+      value: 59,
+    },
+    {
+      date: "2012-12-19",
+      value: 53,
+    },
+    {
+      date: "2012-12-20",
+      value: 54,
+    },
+    {
+      date: "2012-12-21",
+      value: 56,
+    },
+    {
+      date: "2012-12-22",
+      value: 59,
+    },
+    {
+      date: "2012-12-23",
+      value: 58,
+    },
+    {
+      date: "2012-12-24",
+      value: 55,
+    },
+    {
+      date: "2012-12-25",
+      value: 52,
+    },
+    {
+      date: "2012-12-26",
+      value: 54,
+    },
+    {
+      date: "2012-12-27",
+      value: 50,
+    },
+    {
+      date: "2012-12-28",
+      value: 50,
+    },
+    {
+      date: "2012-12-29",
+      value: 51,
+    },
+    {
+      date: "2012-12-30",
+      value: 52,
+    },
+    {
+      date: "2012-12-31",
+      value: 58,
+    },
+    {
+      date: "2013-01-01",
+      value: 60,
+    },
+    {
+      date: "2013-01-02",
+      value: 67,
+    },
+    {
+      date: "2013-01-03",
+      value: 64,
+    },
+    {
+      date: "2013-01-04",
+      value: 66,
+    },
+    {
+      date: "2013-01-05",
+      value: 60,
+    },
+    {
+      date: "2013-01-06",
+      value: 63,
+    },
+    {
+      date: "2013-01-07",
+      value: 61,
+    },
+    {
+      date: "2013-01-08",
+      value: 60,
+    },
+    {
+      date: "2013-01-09",
+      value: 65,
+    },
+    {
+      date: "2013-01-10",
+      value: 75,
+    },
+    {
+      date: "2013-01-11",
+      value: 77,
+    },
+    {
+      date: "2013-01-12",
+      value: 78,
+    },
+    {
+      date: "2013-01-13",
+      value: 70,
+    },
+    {
+      date: "2013-01-14",
+      value: 70,
+    },
+    {
+      date: "2013-01-15",
+      value: 73,
+    },
+    {
+      date: "2013-01-16",
+      value: 71,
+    },
+    {
+      date: "2013-01-17",
+      value: 74,
+    },
+    {
+      date: "2013-01-18",
+      value: 78,
+    },
+    {
+      date: "2013-01-19",
+      value: 85,
+    },
+    {
+      date: "2013-01-20",
+      value: 82,
+    },
+    {
+      date: "2013-01-21",
+      value: 83,
+    },
+    {
+      date: "2013-01-22",
+      value: 88,
+    },
+    {
+      date: "2013-01-23",
+      value: 85,
+    },
+    {
+      date: "2013-01-24",
+      value: 85,
+    },
+    {
+      date: "2013-01-25",
+      value: 80,
+    },
+    {
+      date: "2013-01-26",
+      value: 87,
+    },
+    {
+      date: "2013-01-27",
+      value: 84,
+    },
+    {
+      date: "2013-01-28",
+      value: 83,
+    },
+    {
+      date: "2013-01-29",
+      value: 84,
+    },
+    {
+      date: "2013-01-30",
+      value: 81,
+    },
+  ];
+
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/
+  var chart = root2.container.children.push(
+    am5xy.XYChart.new(root2, {
+      focusable: true,
+      panX: true,
+      panY: true,
+      wheelX: "panX",
+      wheelY: "zoomX",
+      pinchZoomX: true,
+    })
+  );
+
+  var easing = am5.ease.linear;
+
+  // Create axes
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+  var xAxis = chart.xAxes.push(
+    am5xy.DateAxis.new(root2, {
+      maxDeviation: 0.1,
+      groupData: false,
+      baseInterval: {
+        timeUnit: "day",
+        count: 1,
+      },
+      renderer: am5xy.AxisRendererX.new(root2, {}),
+      tooltip: am5.Tooltip.new(root2, {}),
+    })
+  );
+
+  var yAxis = chart.yAxes.push(
+    am5xy.ValueAxis.new(root2, {
+      maxDeviation: 0.2,
+      renderer: am5xy.AxisRendererY.new(root2, {}),
+    })
+  );
+
+  // Add series
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+  var series = chart.series.push(
+    am5xy.LineSeries.new(root2, {
+      minBulletDistance: 10,
+      connect: false,
+      xAxis: xAxis,
+      yAxis: yAxis,
+      valueYField: "value",
+      valueXField: "date",
+      tooltip: am5.Tooltip.new(root2, {
+        pointerOrientation: "horizontal",
+        labelText: "{valueY}",
+      }),
+    })
+  );
+
+  series.fills.template.setAll({
+    fillOpacity: 0.2,
+    visible: true,
+  });
+
+  series.strokes.template.setAll({
+    strokeWidth: 2,
+  });
+
+  // Set up data processor to parse string dates
+  // https://www.amcharts.com/docs/v5/concepts/data/#Pre_processing_data
+  series.data.processor = am5.DataProcessor.new(root2, {
+    dateFormat: "yyyy-MM-dd",
+    dateFields: ["date"],
+  });
+
+  series.data.setAll(data);
+
+  series.bullets.push(function () {
+    var circle = am5.Circle.new(root2, {
+      radius: 4,
+      fill: root2.interfaceColors.get("background"),
+      stroke: series.get("fill"),
+      strokeWidth: 2,
+    });
+
+    return am5.Bullet.new(root2, {
+      sprite: circle,
+    });
+  });
+
+  // Add cursor
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+  var cursor = chart.set(
+    "cursor",
+    am5xy.XYCursor.new(root2, {
+      xAxis: xAxis,
+      behavior: "none",
+    })
+  );
+  cursor.lineY.set("visible", false);
+
+  // add scrollbar
+  chart.set(
+    "scrollbarX",
+    am5.Scrollbar.new(root2, {
+      orientation: "horizontal",
+    })
+  );
+
+  // Make stuff animate on load
+  // https://www.amcharts.com/docs/v5/concepts/animations/
+  chart.appear(1000, 100);
+}); // end am5.ready()
+
+    </script>
     </head>
 
     <body id="page-top">
@@ -190,12 +1187,12 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
                                     <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link"
                                             aria-expanded="false" data-bs-toggle="dropdown" href="#"><span
                                                 class="d-none d-lg-inline me-2 text-gray-600 small title-text"
-                                                id="name"><?php echo $_SESSION['fname'] . ' ' .$_SESSION['lname']  ?></span><img
+                                                id="name"><?php echo $_SESSION['fname'] . ' ' . $_SESSION['lname'] ?></span><img
                                                 class="border rounded-circle img-profile"
                                                 src="assets/img/imported_images/female_profile.svg"></a>
                                         <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in">
                                             <?php
-                                                echo "<a class=\"dropdown-item\" href=\"profile.php?id=$row[id]\"><i class=\"fas fa-user fa-sm fa-fw me-2 text-gray-400\"></i> Profile</a>";
+                                            echo "<a class=\"dropdown-item\" href=\"profile.php?id=$row[id]\"><i class=\"fas fa-user fa-sm fa-fw me-2 text-gray-400\"></i> Profile</a>";
                                             ?>
                                             <div class="dropdown-divider"></div>
                                             <a class="dropdown-item" href="logout.php"><i
