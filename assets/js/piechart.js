06bc498e9190ce8f9b018f0dbc7cd8a7c15212b0
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-27 20:17:18 +0800

    added pie and sales chart

---
+++ assets/js/piechart.js
@@ -0,0 +1,115 @@
+am5.ready(function () {
+  // Create root element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root = am5.Root.new("pie");
+
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root.setThemes([am5themes_Animated.new(root)]);
+
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+  // start and end angle must be set both for chart and series
+  var chart = root.container.children.push(
+    am5percent.PieChart.new(root, {
+      layout: root.verticalLayout,
+      innerRadius: am5.percent(40),
+    })
+  );
+
+  // Create series
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+  // start and end angle must be set both for chart and series
+  var series0 = chart.series.push(
+    am5percent.PieSeries.new(root, {
+      valueField: "bottles",
+      categoryField: "country",
+      alignLabels: false,
+    })
+  );
+
+  var bgColor = root.interfaceColors.get("background");
+
+  series0.ticks.template.setAll({ forceHidden: true });
+  series0.labels.template.setAll({ forceHidden: true });
+  series0.slices.template.setAll({
+    stroke: bgColor,
+    strokeWidth: 2,
+    tooltipText:
+      "{category}: {valuePercentTotal.formatNumber('0.00')}% ({value} bottles)",
+  });
+  series0.slices.template.states.create("hover", { scale: 0.95 });
+
+  var series1 = chart.series.push(
+    am5percent.PieSeries.new(root, {
+      valueField: "litres",
+      categoryField: "country",
+      alignLabels: true,
+    })
+  );
+
+  series1.slices.template.setAll({
+    stroke: bgColor,
+    strokeWidth: 2,
+    tooltipText:
+      "{category}: {valuePercentTotal.formatNumber('0.00')}% ({value} litres)",
+  });
+
+  var data = [
+    {
+      country: "Lithuania",
+      litres: 501.9,
+      bottles: 1500,
+    },
+    {
+      country: "Czech Republic",
+      litres: 301.9,
+      bottles: 990,
+    },
+    {
+      country: "Ireland",
+      litres: 201.1,
+      bottles: 785,
+    },
+    {
+      country: "Germany",
+      litres: 165.8,
+      bottles: 255,
+    },
+    {
+      country: "Australia",
+      litres: 139.9,
+      bottles: 452,
+    },
+    {
+      country: "Austria",
+      litres: 128.3,
+      bottles: 332,
+    },
+    {
+      country: "UK",
+      litres: 99,
+      bottles: 150,
+    },
+    {
+      country: "Belgium",
+      litres: 60,
+      bottles: 178,
+    },
+    {
+      country: "The Netherlands",
+      litres: 50,
+      bottles: 50,
+    },
+  ];
+
+  // Set data
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+  series0.data.setAll(data);
+  series1.data.setAll(data);
+
+  // Play initial series animation
+  // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
+  series0.appear(1000, 100);
+  series1.appear(1000, 100);
+}); // end am5.ready()
--- index.php
+++ index.php
@@ -23,6 +23,15 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
         <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/styles.min.css">
+        <!-- Resources -->
+        <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+        <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+        <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
+        <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+
+        <script src="assets/js/piechart.js"></script>
+        <script src="assets/js/saleschart.js"></script>
+
     </head>
 
     <body id="page-top">
@@ -284,7 +293,7 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
                         </div>
                         <!-- Start: Chart -->
                         <div class="row">
-                            <div class="col-lg-7 col-xl-12">
+                            <div class="col-lg-5 col-xl-7">
                                 <div class="card shadow mb-4 graph-bg">
                                     <div class="card-header d-flex justify-content-between align-items-center header-graph">
                                         <h6 class="text-primary fw-bold m-0 graph-text">Sales Overview</h6>
@@ -302,41 +311,23 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
                                     </div>
                                     <div class="card-body">
                                         <!-- Sales Overview Chart -->
-                                        <div id="chartdiv"></div>
+                                        <!-- HTML -->
+                                        <div id="pie"></div>
+                                        <!-- <div id="chartdiv"></div> -->
                                         <!-- <div class="chart-area"><canvas
                                                 data-bss-chart="{&quot;type&quot;:&quot;line&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;Jan&quot;,&quot;Feb&quot;,&quot;Mar&quot;,&quot;Apr&quot;,&quot;May&quot;,&quot;Jun&quot;,&quot;Jul&quot;,&quot;Aug&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;Earnings&quot;,&quot;fill&quot;:true,&quot;data&quot;:[&quot;0&quot;,&quot;10000&quot;,&quot;5000&quot;,&quot;15000&quot;,&quot;10000&quot;,&quot;20000&quot;,&quot;15000&quot;,&quot;25000&quot;],&quot;backgroundColor&quot;:&quot;rgba(78, 115, 223, 0.05)&quot;,&quot;borderColor&quot;:&quot;rgba(78, 115, 223, 1)&quot;}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;},&quot;scales&quot;:{&quot;xAxes&quot;:[{&quot;gridLines&quot;:{&quot;color&quot;:&quot;rgb(234, 236, 244)&quot;,&quot;zeroLineColor&quot;:&quot;rgb(234, 236, 244)&quot;,&quot;drawBorder&quot;:false,&quot;drawTicks&quot;:false,&quot;borderDash&quot;:[&quot;2&quot;],&quot;zeroLineBorderDash&quot;:[&quot;2&quot;],&quot;drawOnChartArea&quot;:false},&quot;ticks&quot;:{&quot;fontColor&quot;:&quot;#858796&quot;,&quot;fontStyle&quot;:&quot;normal&quot;,&quot;padding&quot;:20}}],&quot;yAxes&quot;:[{&quot;gridLines&quot;:{&quot;color&quot;:&quot;rgb(234, 236, 244)&quot;,&quot;zeroLineColor&quot;:&quot;rgb(234, 236, 244)&quot;,&quot;drawBorder&quot;:false,&quot;drawTicks&quot;:false,&quot;borderDash&quot;:[&quot;2&quot;],&quot;zeroLineBorderDash&quot;:[&quot;2&quot;]},&quot;ticks&quot;:{&quot;fontColor&quot;:&quot;#858796&quot;,&quot;fontStyle&quot;:&quot;normal&quot;,&quot;padding&quot;:20}}]}}}"></canvas>
                                         </div> -->
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-5 col-xl-4">
+                            <div class="col-lg-7 col-xl-12">
                                 <div class="card shadow mb-4 graph-bg">
                                     <div class="card-header d-flex justify-content-between align-items-center header-graph">
                                         <h6 class="text-primary fw-bold m-0 graph-text">Stocks Sources</h6>
-                                        <!-- <div class="dropdown no-arrow"><button class="btn btn-link btn-sm dropdown-toggle"
-                                                aria-expanded="false" data-bs-toggle="dropdown" type="button"><i
-                                                    class="fas fa-ellipsis-v text-gray-400"></i></button>
-                                            <div class="dropdown-menu shadow dropdown-menu-end animated--fade-in">
-                                                <p class="text-center dropdown-header">dropdown header:</p><a
-                                                    class="dropdown-item" href="#">&nbsp;Action</a><a class="dropdown-item"
-                                                    href="#">&nbsp;Another action</a>
-                                                <div class="dropdown-divider"></div><a class="dropdown-item"
-                                                    href="#">&nbsp;Something else here</a>
-                                            </div>
-                                        </div> -->
                                     </div>
                                     <div class="card-body">
-                                        <div id="piechart"></div>
-                                        <!-- <div class="chart-area"> -->
-                                            <!-- <canvas data-bss-chart="{&quot;type&quot;:&quot;doughnut&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;Direct&quot;,&quot;Social&quot;,&quot;Referral&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;&quot;,&quot;backgroundColor&quot;:[&quot;#4e73df&quot;,&quot;#1cc88a&quot;,&quot;#36b9cc&quot;],&quot;borderColor&quot;:[&quot;#ffffff&quot;,&quot;#ffffff&quot;,&quot;#ffffff&quot;],&quot;data&quot;:[&quot;50&quot;,&quot;30&quot;,&quot;15&quot;]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}}}"></canvas> -->
-                                        <!-- </div> -->
-                                        <!-- <div class="text-center small mt-4"><span class="me-2 graph-text"><i
-                                                    class="fas fa-circle text-primary"></i>&nbsp;Direct</span><span
-                                                class="me-2 graph-text"><i
-                                                    class="fas fa-circle text-success"></i>&nbsp;Social</span><span
-                                                class="me-2 graph-text"><i
-                                                    class="fas fa-circle text-info"></i>&nbsp;Refferal</span>
-                                        </div> -->
+                                        
+                                        <div id="sales"></div>
                                     </div>
                                 </div>
                             </div>
