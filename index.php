af3308aefcceef2c9d90ffd9811dba32c2f9b9c8
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-27 18:17:56 +0800

    Added amchart for sales

--- index.php
+++ index.php
@@ -988,12 +988,12 @@ chart.appear(1000, 100);
                         </div>
                         <!-- Start: Chart -->
                         <div class="row">
-                            <div class="col-lg-7 col-xl-8">
+                            <div class="col-lg-7 col-xl-12">
                                 <div class="card shadow mb-4 graph-bg">
                                     <div class="card-header d-flex justify-content-between align-items-center header-graph">
-                                        <h6 class="text-primary fw-bold m-0 graph-text">Earnings Overview</h6>
-                                        <div class="dropdown no-arrow"><button class="btn btn-link btn-sm dropdown-toggle"
-                                                aria-expanded="false" data-bs-toggle="dropdown" type="button"><i
+                                        <h6 class="text-primary fw-bold m-0 graph-text">Sales Overview</h6>
+                                        <!-- <div class="dropdown no-arrow">
+                                            <button class="btn btn-link btn-sm dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button"><i
                                                     class="fas fa-ellipsis-v text-gray-400"></i></button>
                                             <div class="dropdown-menu shadow dropdown-menu-end animated--fade-in">
                                                 <p class="text-center dropdown-header">dropdown header:</p><a
@@ -1002,12 +1002,13 @@ chart.appear(1000, 100);
                                                 <div class="dropdown-divider"></div><a class="dropdown-item"
                                                     href="#">&nbsp;Something else here</a>
                                             </div>
-                                        </div>
+                                        </div> -->
                                     </div>
                                     <div class="card-body">
-                                        <div class="chart-area"><canvas
+                                        <div id="chartdiv"></div>
+                                        <!-- <div class="chart-area"><canvas
                                                 data-bss-chart="{&quot;type&quot;:&quot;line&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;Jan&quot;,&quot;Feb&quot;,&quot;Mar&quot;,&quot;Apr&quot;,&quot;May&quot;,&quot;Jun&quot;,&quot;Jul&quot;,&quot;Aug&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;Earnings&quot;,&quot;fill&quot;:true,&quot;data&quot;:[&quot;0&quot;,&quot;10000&quot;,&quot;5000&quot;,&quot;15000&quot;,&quot;10000&quot;,&quot;20000&quot;,&quot;15000&quot;,&quot;25000&quot;],&quot;backgroundColor&quot;:&quot;rgba(78, 115, 223, 0.05)&quot;,&quot;borderColor&quot;:&quot;rgba(78, 115, 223, 1)&quot;}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;},&quot;scales&quot;:{&quot;xAxes&quot;:[{&quot;gridLines&quot;:{&quot;color&quot;:&quot;rgb(234, 236, 244)&quot;,&quot;zeroLineColor&quot;:&quot;rgb(234, 236, 244)&quot;,&quot;drawBorder&quot;:false,&quot;drawTicks&quot;:false,&quot;borderDash&quot;:[&quot;2&quot;],&quot;zeroLineBorderDash&quot;:[&quot;2&quot;],&quot;drawOnChartArea&quot;:false},&quot;ticks&quot;:{&quot;fontColor&quot;:&quot;#858796&quot;,&quot;fontStyle&quot;:&quot;normal&quot;,&quot;padding&quot;:20}}],&quot;yAxes&quot;:[{&quot;gridLines&quot;:{&quot;color&quot;:&quot;rgb(234, 236, 244)&quot;,&quot;zeroLineColor&quot;:&quot;rgb(234, 236, 244)&quot;,&quot;drawBorder&quot;:false,&quot;drawTicks&quot;:false,&quot;borderDash&quot;:[&quot;2&quot;],&quot;zeroLineBorderDash&quot;:[&quot;2&quot;]},&quot;ticks&quot;:{&quot;fontColor&quot;:&quot;#858796&quot;,&quot;fontStyle&quot;:&quot;normal&quot;,&quot;padding&quot;:20}}]}}}"></canvas>
-                                        </div>
+                                        </div> -->
                                     </div>
                                 </div>
                             </div>
