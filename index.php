c46ce6faf4df3ada10a9fbfb8e28c804500395d8
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-28 11:25:29 +0800

    fixed issue for email not set

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ session_start(); // start session with the user who logged in
 
 require_once("connect_db.php"); // include connection to database
 
-$sql = "SELECT * FROM users WHERE email = '" . $_SESSION['email'] . "'"; // query to select the user who logged in
-$result = mysqli_query($conn, $sql); // execute the query
-$row = mysqli_fetch_assoc($result); // fetch the result, or data in a row
+?>
+
+<?php
 
 // fetch standard shipping sales
 $sql_strd = "SELECT * FROM sales where shipment = 'Standard'";
@@ -78,7 +78,12 @@ $date = $row_sale['date']; // store date in variable
 // }
 // }
 // if user is logged in, it means that the session variables are set or contain values. Then display the dashboard
+?>
+<?php
 if (isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['email'])) {
+    $sql = "SELECT * FROM users WHERE email = '" . $_SESSION['email'] . "'"; // query to select the user who logged in
+    $result = mysqli_query($conn, $sql); // execute the query
+    $row = mysqli_fetch_assoc($result); // fetch the result, or data in a row
 
     ?>
     <!DOCTYPE html>
