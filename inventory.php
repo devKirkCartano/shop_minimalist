330300abefd81e637e7397b7fa776e87144b156e
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2023-07-28 16:39:57 +0800

    change title for charts

--- inventory.php
+++ inventory.php
@@ -52,9 +52,9 @@ if(isset($_SESSION['fname']) && isset($_SESSION['lname']) && isset($_SESSION['em
                                 <path
                                     d="M13,10h-2V8h2V10z M13,6h-2V1h2V6z M7,18c-1.1,0-1.99,0.9-1.99,2S5.9,22,7,22s2-0.9,2-2S8.1,18,7,18z M17,18 c-1.1,0-1.99,0.9-1.99,2s0.89,2,1.99,2s2-0.9,2-2S18.1,18,17,18z M8.1,13h7.45c0.75,0,1.41-0.41,1.75-1.03L21,4.96L19.25,4l-3.7,7 H8.53L4.27,2H1v2h2l3.6,7.59l-1.35,2.44C4.52,15.37,5.48,17,7,17h12v-2H7L8.1,13z">
                                 </path>
-                            </svg><span>Sales</span></a></li>
+                            </svg><span>Sales Inventory</span></a></li>
                         <li class="nav-item"><a class="nav-link active icon-color" href="inventory.php"><i
-                                class="far fa-list-alt icon-color"></i><span>Inventory List</span></a></li>
+                                class="far fa-list-alt icon-color"></i><span>Product Inventory</span></a></li>
 
                 </ul>
                 <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0"
